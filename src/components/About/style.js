b5f197804c632108341f41819a72c7414fc090eb
Vinicius <viniciusassis.dev@hotmail.com>
2020-09-27 15:11:08 -0300

    Create Paragraph wrapper and add styled in components

--- src/components/About/style.js
+++ src/components/About/style.js
@@ -4,15 +4,29 @@ import backGround from '../../assets/bg.jpg'
 export const AboutWrapper = styled.section`
   width: 100vw;
   height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   background: url(${backGround}) no-repeat 20%;
   background-size: cover;
 `
 
 export const AbouTitle = styled.h1`
-  font-size: 2.4rem;
+  font-size: 3.6rem;
   font-family: 'Robot', sans-serif;
   font-style: italic;
+  text-align: center;
+  margin-bottom: 2.2rem;
 `
+
 export const AboutParagraph = styled.p`
-  font-size: 1.4rem;
+  font-size: 2.2rem;
+`
+
+export const ParagraphWrapper = styled.div`
+  width: 100%;
+  height: auto;
+  padding: 20px 80px;
+  text-align: center;
+  background-color: rgba(0,0,0,0.3);
 `
