d1585db8ec2510ec4e288f3bfd26086aeb35462f
Vinicius <viniciusassis.dev@hotmail.com>
2020-09-27 14:30:17 -0300

    Create About component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+// reset css
 import GlobalStyles from './styles/GlobalStyles'
+
+//components
+import About from './components/About/index'
 
 
 const App = () => 
 <>
   <GlobalStyles />
+  <About />
   <h1>Harry Finder!</h1>
 </>
 
---
+++ src/components/About/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import backGround from '../../assets/bg.jpg'
+
+const About = styled.section`
+  width: 100vw;
+  height: 100vh;
+  background: url(${backGround}) no-repeat 20%;
+  background-size: cover;
+`
+
+export default About
