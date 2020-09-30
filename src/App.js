a94bbbc8ac46dea94367fd4c99b67fa30548e5f2
Vinicius <viniciusassis.dev@hotmail.com>
2020-09-30 00:34:40 -0300

    create functionality to get data from api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useContext } from 'react';
+import React from 'react';
 // reset css
 import GlobalStyles from './styles/GlobalStyles'
-import { GlobalContext, GlobalStorage } from './context/GlobalContext/index'
+import { GlobalStorage } from './context/GlobalContext/index'
 
 //components
 import Menu from './components/Menu';
@@ -10,16 +10,14 @@ import About from './components/About/index'
 import Characters from './components/Characters';
 import Movies from './components/Movies';
 
-import { ContentItem } from './components/MobileMenu/style'
-
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 
 const App = () => (
   <GlobalStorage>
     <GlobalStyles />
-    <Router>
     <Menu />
+    <Router>
     <MobileMenu />
       <Switch>
         <Route path="/" exact render={() => <About />}/>
--- src/components/Characters/index.js
+++ src/components/Characters/index.js
@@ -1,16 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { SearchButton, SearchField, SearchWrapper } from './style'
 import { ReactComponent as SearchIcon} from '../../assets/search.svg'
 
 
-const Characters = () => (
-  <SearchWrapper>
-    <SearchField />
-    <SearchButton>
-      <SearchIcon />
-    </SearchButton>
-  </SearchWrapper>
-)
+const Characters = () => {
+  const [inputValue, setInputValue] = useState('') 
+  const [response, setResponse] = useState(null)
+  const [isFetching, setIsFetching] = useState(false)
+
+  const capitalizeName = (name) => {
+  return name.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  }
+
+  const getApi = async (character) => {
+    const person = capitalizeName(character).replace(' ', '%20')
+    try {
+      setIsFetching(true)
+      const url = `https://www.potterapi.com/v1/characters/?key=%242a%2410%24pZzh7vkBfH9jIDq6hrTSa.Qf%2Fwm2F6AkUgO4uol9g20o8yECBe8Iq&name=${person}`
+      const response = await fetch(url)
+      const responseJson = await response.json()
+      setResponse(...responseJson)
+    }
+    catch (error) {
+      console.error(error)
+    } finally {
+      setIsFetching(false)
+    }
+  }
+
+  function handleChange({target}) { 
+      setInputValue(target.value)
+  }
+
+  function handleClick() {
+    getApi(inputValue)
+  }
+
+  return (
+    <SearchWrapper>
+      <SearchField onChange={handleChange} onKeyDown={({key}) => key === 'Enter' ? handleClick() : ''}/>
+      <SearchButton onClick={handleClick}>
+        <SearchIcon />
+      </SearchButton>
+      {isFetching && <p>Carregando...</p>}
+      {!!response && <h1>{response.name}</h1>}
+    </SearchWrapper>  
+  )
+}
 
 export default Characters
